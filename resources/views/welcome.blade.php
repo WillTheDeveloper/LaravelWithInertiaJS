c324f5ff867116699ac696e0407012328c9280f6
Will <will@teamatlas.cloud>
2021-08-05 18:23:06 +0100

    Uhhhh, its messy

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<head>
+    <!-- ... --->
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <script src="{{ mix('/js/app.js') }}" defer></script>
+</head>
+
+<body>
+    @inertia
+    <h1>Hello there!</h1>
+</body>
